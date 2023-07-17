37a53f1134e9e4baba4de9b8baba912416bfc500
Bhuvaneswari <mailtobhuvana04@gmail.com>
2023-07-17 11:56:09 -0700

    A sample program area of square

--- src/main/java/LargestNumber.java
+++ src/main/java/LargestNumber.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class LargestNumber {
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter a=");
+        int a=in.nextInt();
+        System.out.println("Enter B");
+        int b=in.nextInt();
+        System.out.println("Enter C=");
+        int c=in.nextInt();
+        int max=a;
+//        if(b>max)
+//        {
+//            max=b;
+//        }
+//        if(c>max)
+//        {
+//            max=c;
+//        }
+        max=Math.max(c,Math.max(a,b));
+        System.out.println(max);
+    }
+
 }
